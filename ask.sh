0ae01b63f7776ba5d03279be8f22d1fdb4025001
messagecompass <messagecompass@gmail.com>
2025-04-01 14:48:59 -0400

    escape quotes

--- ask.sh
+++ ask.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-
 CLIPBOARD_TEXT=$(xclip -o -selection clipboard 2>/dev/null)
 
 if [ -n "$CLIPBOARD_TEXT" ]; then
-  gnome-terminal -- bash -c "echocmd.sh -a \"$CLIPBOARD_TEXT\"; read -p 'Press Enter to close...'; exit"
-else
+  CLIPBOARD_ESCAPED=$(printf '%s\n' "$CLIPBOARD_TEXT" | sed 's/["]/\\&/g')
+  gnome-terminal -- bash -c "echocmd.sh -a \"$CLIPBOARD_ESCAPED\"; read -p 'Press Enter to close...'; exit"
+ee
   echo "nothing in clipboard to ask"
 fi
 
